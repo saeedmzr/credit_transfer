c3083f994eea9f45e293662a8156321282d0a2c1
saeed <saeedmouzarmi@gmail.com>
2024-10-13 19:44:13 +0330

    Added create wallet service.

--- apps/wallets/services.py
+++ apps/wallets/services.py
@@ -1,6 +1,9 @@
 from django.db import transaction
 
+from apps.base import helpers
 from apps.base.services import BaseService
+from apps.crypto.models import Crypto
+from apps.users.models import User
 from apps.wallets.models import Wallet, WalletLog, WalletLogType
 from apps.wallets.repositories import WalletRepository, WalletLogRepository, DepositRepository, TransferRepository
 from apps.wallets.serializers import DepositSerializer
@@ -8,6 +11,11 @@ from apps.wallets.serializers import DepositSerializer
 
 class WalletService(BaseService):
     _repository = WalletRepository
+
+    @classmethod
+    def create_wallet(cls, data):
+        data['hash'] = helpers.generate_random_string()
+        super().create(data=data)
 
 
 class TransferService(BaseService):
