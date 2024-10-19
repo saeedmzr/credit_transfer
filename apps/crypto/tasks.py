4523fa70a4044f3c82c219c5ab70abe2f1c16819
saeed <saeedmouzarmi@gmail.com>
2024-10-19 14:52:55 +0330

    Added tasks for crypto

--- apps/crypto/tasks.py
+++ apps/crypto/tasks.py
@@ -11,34 +11,29 @@ from .serializers import CryptoSerializer
 
 
 @shared_task
-def fetch_crypto_list():
+def fetch_crypto_prices():
     crypto_fetcher = CryptoFetcher(type="fcs")
-    crypto_list = crypto_fetcher.fetcher.get_crypto_list()
-
-    for crypto in crypto_list:
-        CryptoRepository.create_or_update(
-            insert_data={
-                "abbreviation": crypto.get("symbol")
-            },
-            update_data={
-                "name": crypto.get("name")
-            }
-        )
-    return crypto_list
+    items = CryptoRepository.get_items_for_updating_price()
+    for crypto in items:
+        result = crypto_fetcher.fetcher.get_crypto_price(crypto.abbreviation)
+        if result.get("code", 0) == 200:
+            price_item = result.get("response")[0]
+            crypto.price = price_item.get("c") or price_item.get("o")
+            crypto.save()
 
 
 @shared_task
 def fetch_crypto_list():
     crypto_fetcher = CryptoFetcher(type="fcs")
-    crypto_list = crypto_fetcher.fetcher.get_crypto_list()
-
-    for crypto in crypto_list:
-        CryptoRepository.create_or_update(
-            insert_data={
-                "abbreviation": crypto.get("symbol")
-            },
-            update_data={
-                "name": crypto.get("name")
-            }
-        )
-    return crypto_list
+    result = crypto_fetcher.fetcher.get_crypto_list()
+    if result.get("code", 0) == 200:
+        crypto_list = result.get("response")
+        for crypto in crypto_list:
+            CryptoRepository.create_or_update(
+                insert_data={
+                    "abbreviation": crypto.get("symbol")
+                },
+                update_data={
+                    "name": crypto.get("name")
+                }
+            )
