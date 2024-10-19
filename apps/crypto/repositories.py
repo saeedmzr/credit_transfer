08065842c46a2c80b1b726551e683057272c0af3
saeed <saeedmouzarmi@gmail.com>
2024-10-19 14:52:41 +0330

    Added a method for updating crypto prices

--- apps/crypto/repositories.py
+++ apps/crypto/repositories.py
@@ -7,3 +7,7 @@ class CryptoRepository(BaseRepository):
     _model = Crypto
     _serializer = CryptoSerializer
 
+    @classmethod
+    def get_items_for_updating_price(cls):
+        items = cls._model.objects.all().order_by("updated_at")[:10]
+        return items
