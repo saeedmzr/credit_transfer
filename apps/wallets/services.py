b3e8ec9ed816d1a542a86ec4b1223addb54c1586
saeed <saeedmouzarmi@gmail.com>
2024-10-20 18:35:35 +0330

    Changed logic for deposit

--- apps/wallets/services.py
+++ apps/wallets/services.py
@@ -2,11 +2,8 @@ from django.db import transaction
 
 from apps.base import helpers
 from apps.base.services import BaseService
-from apps.crypto.models import Crypto
-from apps.users.models import User
-from apps.wallets.models import Wallet, WalletLog, WalletLogType
+from apps.wallets.models import WalletLogType, TransferStatus
 from apps.wallets.repositories import WalletRepository, WalletLogRepository, DepositRepository, TransferRepository
-from apps.wallets.serializers import DepositSerializer
 
 
 class WalletService(BaseService):
@@ -15,16 +12,66 @@ class WalletService(BaseService):
     @classmethod
     def create_wallet(cls, data):
         data['hash'] = helpers.generate_random_string()
-        super().create(data=data)
+        return super().create(data=data)
+
+
+class WalletLogService(BaseService):
+    _repository = WalletLogRepository
 
 
 class TransferService(BaseService):
     _repository = TransferRepository
+    _wallet_service = WalletService()
+    _wallet_log_service = WalletLogService()
+
+    @classmethod
+    def create(cls, data: dict):
+        transfer = super().create(data=data)
+
+        with transaction.atomic():
+            sender_wallet = cls._wallet_service.get_and_lock_for_update(transfer.sender.hash)
+            receiver_wallet = cls._wallet_service.get_and_lock_for_update(transfer.receiver.hash)
+            cls._wallet_service.update(sender_wallet.hash, {
+                'balance': sender_wallet.balance - transfer.amount
+            })
+            cls._wallet_service.update(receiver_wallet.hash, {
+                'balance': receiver_wallet.balance + transfer.amount
+            })
+
+            cls._wallet_log_service.create(data={
+                'wallet': sender_wallet,
+                'amount': transfer.amount,
+                'balance': sender_wallet.balance - transfer.amount,
+                'type': WalletLogType.TRANSFER
+            })
+            cls._wallet_log_service.create(data={
+                'wallet': receiver_wallet,
+                'amount': transfer.amount,
+                'balance': receiver_wallet.balance + transfer.amount,
+                'type': WalletLogType.DEPOSIT
+            })
+            cls.update(transfer.id, {'status': TransferStatus.DONE})
 
 
 class DepositService(BaseService):
     _repository = DepositRepository
+    _wallet_service = WalletService()
+    _wallet_log_service = WalletLogService()
 
-    def deposit(self, data):
-        deposit = self._repository.create(data)
-        return deposit
+    @classmethod
+    def create(cls, data: dict):
+        deposit = super().create(data=data)
+
+        with transaction.atomic():
+            wallet = cls._wallet_service.get_and_lock_for_update(deposit.wallet.hash)
+            cls._wallet_service.update(wallet.hash, {
+                'balance': wallet.balance + deposit.amount
+            })
+
+            cls._wallet_log_service.create(data={
+                'wallet': wallet,
+                'amount': deposit.amount,
+                'balance': wallet.balance + deposit.amount,
+                'type': WalletLogType.DEPOSIT
+            })
+            cls.update(deposit.id, {'status': TransferStatus.DONE})
